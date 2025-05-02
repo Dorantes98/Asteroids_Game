07f0aa98de40257a35591ca05aebc4f2d5cc1ddb
Dorantes98 <dominickadorantes98@gmail.com>
2025-05-02 13:30:03 -0700

    added 60 fps cap for fame, added circleshape class

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ def main ():
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
